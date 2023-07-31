9a27efb7882eb1ed3d1bed1ea71577a717c2946a
Michal Palko <Michal.Palko1@ibm.com>
2023-07-31 14:43:40 +0200

    content mostly

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ themeToggle.addEventListener('change', () => {
       event_category: 'theme',
       event_label: 'light_theme',
     });
+    getInfoAndWriteToCloudant();
     body.classList.remove('light-mode'); // remove light mode
     body.classList.add('dark-mode'); // add dark mode
     updateThemeLabel('dark-mode');
@@ -94,19 +95,7 @@ function trackClickEvent(socialMedia) {
     }
   }
   
-  function refreshFirstTime() {
-    if (!localStorage.getItem("firstVisit")) {
-      // Set the flag in localStorage to indicate it's the first visit
-      localStorage.setItem("firstVisit", "true");
-  
-      // Perform the reload
-      window.location.reload();
-      console.log("page reloaded");
-    }
-  }
 
-
-  
   const button = document.querySelector(".collapsible-button");
   const content = document.querySelector(".collapsible-content");
 
@@ -159,3 +148,59 @@ function trackClickEvent(socialMedia) {
   //   }
   // });
 
+
+
+    // Function to send data to Cloudant
+    function writeToCloudant(data) {
+      const cloudantURL = 'https://18a02f06-b509-413d-a320-dcb7a06c38c9-bluemix.cloudantnosqldb.appdomain.cloud/'; // Replace with your Cloudant URL
+      const username = 'apikey-300051e017cf4be6bfae3dc7a675868e'; // Replace with your Cloudant username
+      const password = '5ae1a88eda0156917c938e7e14fa3150d711016e'; // Replace with your Cloudant password
+      const databaseName = 'resume'; // Replace with your database name
+  
+      // Create the basic auth header
+      const basicAuth = btoa(username + ':' + password);
+  
+      // Construct the request options
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Basic ' + basicAuth,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      };
+  
+      // Make the API call to Cloudant
+      fetch(`${cloudantURL}/${databaseName}`, requestOptions)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to write data to Cloudant');
+          }
+          return response.json();
+        })
+        .then(responseData => {
+          console.log('Data written successfully:', responseData);
+        })
+        .catch(error => {
+          console.error('Error writing data to Cloudant:', error);
+        });
+    }
+  
+    // Function to get information about user's computer
+    function getInfoAndWriteToCloudant() {
+      // Modify this function to gather information about the user's computer.
+      // For example, you can use libraries like Fingerprintjs or detect.js to get more detailed information.
+      const userComputerInfo = {
+        userAgent: navigator.userAgent,
+        screenResolution: `${screen.width}x${screen.height}`,
+        language: navigator.language,
+        // Add more properties as needed.
+      };
+  
+      // Call the function to write data to Cloudant
+      writeToCloudant(userComputerInfo);
+      console.log('data sent')
+    }
+  
+    // Call the function when the page loads
+    getInfoAndWriteToCloudant();
